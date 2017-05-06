23a8fd5687097008a0982b19f0553f7248c566e3
zhankui.hu <reoszo@126.com>
2017-05-06 10:08:46 +0800

    增加format

--- README.md
+++ README.md
@@ -8,13 +8,44 @@ DateTime 静态方法也类似 Date，只是返回的是 DateTime对象
     getDaysInMonth(year, month)
     getDaysInYear(year)
 
-
-功能参考 XDate
-
 DateTime.prototype = {
     clone
     toDate
     format
+        y: 年份 1(0001) 2017(2017)
+        yy: 年份的后两位（着实没有用处），不够两位补零 01(0001) 17(2017)
+        yyyy: 年份的四位表示 0001(0001) 2017(2017)
+        M: 月份 1 ... 12
+        MM: 月份 01 ... 12
+        MMM: Jan ... Dec
+        MMMM: January ... December
+        d: 1 ... 31
+        dd: 01 ... 31
+        <!--D: 1 ... 365-->
+        <!--DDD: 001 ... 365-->
+        e: 星期 0 ... 6
+        ee: Su ... Sa
+        eee: Sun ... Sat
+        eeee: Sunday ... Saturday
+        h: 12小时制 1 ... 12
+        hh: 12小时制 01 ... 12
+        H: 24小时制 0 ... 23
+        HH: 24小时制 00 ... 23
+        k: 24小时制 1 ... 24
+        kk: 24小时制 01 ... 24
+        K: 12小时制 0 ... 11
+        KK: 12小时制 00 ... 11
+        m: 分钟 0 ... 59
+        mm: 分钟 00 ... 59
+        s: 秒 0 ... 59
+        ss: 秒 00 ... 59
+        x: 毫秒时间戳 1494035952079
+        X: 秒时间戳 1494035952
+        S: 一位毫秒表示 0 ... 9 （5）
+        SS: 两位毫秒表示 00 ... 99 （56）
+        SSS: 三位毫秒表示 00 ... 999 （567）
+        a: am pm
+        A: AM PM
     
     addYears // 自动处理溢出
     addMonths // 自动处理溢出，例如：7月31加一个月为8月30
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const MILLISECONDS = 1000,
     MINUTE_MILLISECONDS = 60 * MILLISECONDS,
     HOUR_MILLISECONDS = 60 * MINUTE_MILLISECONDS,
     DAY_MILLISECONDS = 24 * HOUR_MILLISECONDS,
-    WEEK_MILLISECONDS = 7 * DAY_MILLISECONDS;
+    WEEK_MILLISECONDS = 7 * DAY_MILLISECONDS,
+    WEEK_NAMES = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    MONTH_NAMES = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
 function diff(date1, date2, unit) {
     let offset = date1.getTimezoneOffset() * MINUTE_MILLISECONDS;
@@ -10,6 +12,29 @@ function diff(date1, date2, unit) {
 }
 
 class DateTime extends Date {
+    // begin 快捷日期，好像没啥用
+    static today() {
+        return new DateTime();
+    }
+    static beforeYesterday() {
+        return new DateTime().addDay(-2);
+    }
+    static beforeYestoday() {
+        return new DateTime().addDay(-2);
+    }
+    static yesterday() {
+        return new DateTime().addDay(-1);
+    }
+    static yestoday() {
+        return new DateTime().addDay(-1);
+    }
+    static tomorrow() {
+        return new DateTime().addDay(1)
+    }
+    static afterTomorrow() {
+        return new DateTime().addDay(2)
+    }
+    // end 快捷日期，好像没啥用
     static isLeapYear(year) {
         return (years % 4 == 0 && years % 100 != 0) || years % 400 == 0;
     }
@@ -21,10 +46,59 @@ class DateTime extends Date {
         return DateTime.isLeapYear(year) ? 366 : 365;
     }
     static getMonthCalendar(year, month, option) {
-
+        // xxx
     }
     static format(date, pattern) {
-        return 'yyyy-MM-dd HH:mm:ss'
+        if (!(date instanceof Date)) {
+            date = new Date();
+        }
+        return pattern.replace(/'(.)|([a-z])\2*/ig, function (match, escapeChar, formatChar, index, pattern) {
+            if (escapeChar) {
+                return escapeChar
+            }
+            switch (match) {
+                case 'y': return date.getFullYear();
+                case 'yy': return date.getFullYear().toString().slice(-2).toString().padStart(2, '0');
+                case 'yyyy': return date.getFullYear().toString().padStart(4, '0');
+                case 'M': return date.getMonth() + 1;
+                case 'MM': return (date.getMonth() + 1).toString().padStart(2, '0');
+                case 'MMM': return MONTH_NAMES[date.getMonth()].slice(0, 3);
+                case 'MMMM': return MONTH_NAMES[date.getMonth()];
+                case 'd': return date.getDate();
+                case 'dd': return date.getDate().toString().padStart(2, '0');
+                // case 'D': return 365;
+                // case 'DDD': return 365;
+                case 'e': return date.getDay();
+                case 'ee': return WEEK_NAMES[date.getDay()].slice(0, 2);
+                case 'eee': return WEEK_NAMES[date.getDay()].slice(0, 3);
+                case 'eeee': return WEEK_NAMES[date.getDay()];
+                case 'h': return date.getHours() % 12 || 12;
+                case 'hh': return (date.getHours() % 12 || 12).toString().padStart(2, '0');
+                case 'H': return date.getHours();
+                case 'HH': return date.getHours().toString().padStart(2, '0');
+                case 'k': return date.getHours() || 24;
+                case 'kk': return (date.getHours() || 24).toString().padStart(2, '0');
+                case 'K': return date.getHours() % 12;
+                case 'KK': return (date.getHours() % 12).toString().padStart(2, '0');
+                case 'm': return date.getMinutes();
+                case 'mm': return date.getMinutes().toString().padStart(2, '0');
+                case 's': return date.getSeconds();
+                case 'ss': return date.getSeconds().toString().padStart(2, '0');
+                case 'x': return date.getTime();
+                case 'X': return date.getTime().toString().slice(0, -3);
+                case 'S': return date.getMilliseconds().toString().padStart(3, '0').slice(0, 1);
+                case 'SS': return date.getMilliseconds().toString().padStart(3, '0').slice(0, 2);
+                case 'SSS': return date.getMilliseconds().toString().padStart(3, '0');
+                case 'a': return date.getHours() < 12 ? 'am' : 'pm';
+                case 'A': return date.getHours() < 12 ? 'AM' : 'PM';
+                // case 'DDD': return '年的第几天';
+                // case 'w': return '年的第几周';
+                // case 'W': return '月的第几周';
+                // case 'z': return '时区';
+                // case 'Z': return '时区';
+                default: return match;
+            }
+        });
     }
 
     constructor(...args) {
@@ -45,7 +119,7 @@ class DateTime extends Date {
     getYearDays() {
         return DateTime.getYearDays(this.getFullYear());
     }
-    addYear(years) {
+    addYear(years = 1) {
         let year = this.getFullYear(),
             month = this.getMonth(),
             day = this.getDate(),
@@ -56,7 +130,7 @@ class DateTime extends Date {
             diffMilliseconds = Date.UTC(toYear, toMonth, toDay) - Date.UTC(year, month, day);
         this.addMillisecond(diffMilliseconds);
     }
-    addMonth(months) {
+    addMonth(months = 1) {
         let year = this.getFullYear(),
             month = this.getMonth(),
             day = this.getDate();
@@ -68,22 +142,22 @@ class DateTime extends Date {
             diffMilliseconds = Date.UTC(toYear, toMonth, toDay) - Date.UTC(year, month, day);
         this.addMillisecond(diffMilliseconds);
     }
-    addWeek(weeks) {
+    addWeek(weeks = 1) {
         this.addMillisecond(weeks * WEEK_MILLISECONDS);
     }
-    addDay(days) {
+    addDay(days = 1) {
         this.addMillisecond(days * DAY_MILLISECONDS);
     }
-    addHour(hours) {
+    addHour(hours = 1) {
         this.addMillisecond(hours * HOUR_MILLISECONDS);
     }
-    addMinute(minutes) {
+    addMinute(minutes = 1) {
         this.addMillisecond(minutes * MINUTE_MILLISECONDS);
     }
-    addSecond(seconds) {
+    addSecond(seconds = 1) {
         this.addMillisecond(seconds * MILLISECONDS);
     }
-    addMillisecond(milliseconds) {
+    addMillisecond(milliseconds = 1) {
         this.setTime(this.getTime() + milliseconds);
     }
     diffYear(date) {
